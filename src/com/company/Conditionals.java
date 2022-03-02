d9cb1ef085863dce317abfb0ee444eafc2e6c235
karobalt <k.varsockaja@gmail.com>
2022-03-02 21:13:12 +0200

    conditionals and loops

---
+++ src/com/company/Conditionals.java
@@ -0,0 +1,136 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        // Conditionals is simply a way of telling the computer to perform certain actions based on certain conditions.
+//        int number = 3;
+//        if (number==2) {
+//            System.out.println("The value is 2");}
+//        else {
+//            System.out.println("The value is not 2");
+//        }
+
+//        int number = 8;
+//        if (number>9) {
+//            System.out.println("The number " + number + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//        int number1 = 9;
+//        if (number1>9) {
+//            System.out.println("The number " + number1 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//        int number2 = 10;
+//        if (number2>9) {
+//            System.out.println("The number " + number2 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//
+//        int number3 = 11;
+//        if (number3>9) {
+//            System.out.println("The number " + number3 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//
+//        int number4 = 12;
+//        if (number4>9) {
+//            System.out.println("The number " + number4 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//        int number5 = 13;
+//        if (number5>9) {
+//            System.out.println("The number " + number5 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//        int number6 = 14;
+//        if (number6>9) {
+//            System.out.println("The number " + number6 + " is greater than 9");}
+//        else {
+//            System.out.println("Did not satisfy the task");
+//        }
+//    }
+
+//        int value = 11;
+//        if (value == 11) {
+//            System.out.println(value + " is eleven");
+//        } else if (value > 9 && value < 15) {
+//            System.out.println(value + " is greater than 9");
+//        } else if (value<9){
+//            System.out.println(value + " is not greater than 9");
+//        } else {
+//            System.out.println(value + "value is out of bounds");
+
+//        }
+
+        // Loops are construct that allows us to repeat a set of commands
+        // until a condition to terminate ir reached.
+        // Do-While loop is basically saying do this for as long as the condition is true.
+//        int i = 0;
+//        do {
+//            System.out.println(i); // print out the count
+//            i=i+1; // increase the count  by 1
+//        } while (i<10);
+
+        // while loop is basically saying do this
+        // PRO-TIP: user while loop when you don't have and idea of how many
+        // times the loop is supposed to run.
+//        while (i<11) {
+//            System.out.println(i);
+//            i=i+1;
+//        }
+
+        // For loop
+        // Use the for loop typically when you have an idea of how many times
+        // the loop is supposed to run.
+//        for (int i = 0; i< 15; i++) {
+//            if (i > 9){
+//                System.out.println(i+" is greater than 9");
+//            }
+
+//        for (int i=1; i<51; i++) {
+//            if( i%2==0) {
+//                System.out.println(i +" is even");
+//            }
+//            else {
+//                System.out.println(i+ " is odd");
+//            }
+//        }
+//        int i=1;
+//        while (i<51) {
+//            if( i%4==0) {
+//                System.out.println(i +" is a multiple of 4");
+//            }
+//            i=i+1;
+//        }
+
+
+        // Unary operators:
+        // Addition
+        // pre-incrementation operators ++i
+        // post-incrementation operators i++
+        // Subtraction
+        // pre-incrementation operators --i
+        // post-incrementation operators i--
+
+        for (int i = 1; i < 51; i++) {
+            if (i % 3 == 0  && i % 7 ==0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 7 == 0 ) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    // java is an object oriented programming language that allows us to model
-        // real lifa functionality as objects in our code.
+        // real life functionality as objects in our code.
 
         // Features of Java
         // java is a strictly typed language
@@ -25,6 +25,7 @@ public class Main {
         // classes
 
         // Variables and Datatypes
+        // Numeric primitive types
         // int (Integer)
         // short
         // byte
@@ -32,6 +33,10 @@ public class Main {
         // float // has a max of 7 decimal places
         // double // has a max of 15 decimal places
         // Strings
+
+        // Non-numeric primitives
+        // char - characters.
+        // boolean - true or false values
 
         // Variable: Think of it as of an empty box, where as soon as you create a box
         // you decide what type of items you can put in the box and you cannot put a different type
@@ -42,10 +47,10 @@ public class Main {
         // A variable declaration is simply giving your variable a name and data type.
         // A variable initialization is the point where you finally give that variable a value to start with.
 
-        float number; // variable declaration
-        number = 4; // variable initialization
+//        float number; // variable declaration
+//        number = 4; // variable initialization
 
-        float value = 6; // variable declaration and initialization in one line.
+//        float value = 6; // variable declaration and initialization in one line.
 //        byte byteValue = 1;
 
 //        char charValue = 'u'; // accepts number by ASCII
@@ -58,19 +63,73 @@ public class Main {
 
         //Arithmetic operators include:
         // + addition
-        System.out.println("Addition of number value is:" + (number + value));
+//        System.out.println("Addition of number value is:" + (number + value));
         // - subtraction
-        System.out.println("Subtraction of number value is:" + (number - value));
+//        System.out.println("Subtraction of number value is:" + (number - value));
         // / division
-        System.out.println("Division of number value is:" + (number / value));
+//        System.out.println("Division of number value is:" + (number / value));
         // * multiplication
-        System.out.println("Multiplication of number value is:" + (number * value));
+//        System.out.println("Multiplication of number value is:" + (number * value));
         // % modulus ( it divides two values and returns the remainder)
-        System.out.println("Modulus of number value is:" + (number % value));
+//        System.out.println("Modulus of number value is:" + (number % value));
 
-        // Shortcut to  comment a line of cide: CTRL + /
+        // Shortcut to  comment a line: CTRL + /
 
 //        System.out.println("Hello World!");
+
+        //Type conversion
+        // Java allows you to implicitly convert from a smaller data type to a bigger one,
+        // but not the other way around.
+        // For you to convert from bigger to smaller you must do it explicitly.
+        // CASTING
+        // smaller -> bigger
+//        int five = 5;
+//        long newValue = five;
+
+        //bigger -> smaller
+//        long six = 6;
+//        int intValue = (int)six;
+
+//        double deciNumber = 6.892323223;
+//        System.out.println("Double value:" + deciNumber);
+//        int truncatedValue = (int)deciNumber;
+//        System.out.println("Integer version:" + truncatedValue);
+//        boolean bool = true;
+//        boolean loob = 8>5;
+//        System.out.println(loob);
+
+        // A boolean is simply a statement or expression that results in true or false.
+        // Relational operators:
+        // >
+        // <
+        // == equals to
+        // >=
+        // <=
+
+//        System.out.println(5 >= 9);
+
+        // Logical operators:
+        // && - and operator
+//        System.out.println(5 >= 9 && 6==6); // both must be satisfied, else false.
+        // || - or operator
+//        System.out.println(5 >= 9 || 6==6); // at least one is supposed to be satisfied
+
+//        System.out.println(6<9 && 9>2);
+//        System.out.println(5>9 || 8<3);
+//        System.out.println(8==2 && 7<6);
+
+
+
+//        int age;
+//        age = 20;
+//        String gender;
+//        gender = "Female";
+//        float height = 1.70f;
+//        float weight = 70.5f;
+//        System.out.println("Users age: " + age);
+//        System.out.println("Users gender: " + gender);
+//        System.out.println("Users height: " + height + " m");
+//        System.out.println("Users weight: " + weight + " kg");
 
 
     }
